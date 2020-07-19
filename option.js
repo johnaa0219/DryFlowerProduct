dd4c7722e7b30bbe36ff5fe6f1786f0833d7b3e4
johnaa0219 <51010495+johnaa0219@users.noreply.github.com>
2020-07-19 22:51:00 +0800

    Add files via upload

--- option.js
+++ option.js
@@ -16,68 +16,65 @@ var checkNum = document.querySelector('.checknum');
 var checkoutBack = document.querySelector('.checkoutback');
 var checkButton = document.querySelector('.checkbutton');
 var final = [{"name" : "" , "src" : "" , "price" : 0 , "num" : 0}];
-LoadCommodity();
-async function LoadCommodity(){
-    await fetch('commodity.json')
-    .then(await function(response){
-        return response.json();
-    })
-    .then(await function(myJson){
-        for(var i=0; i<myJson.length; i++){
-            var node = document.createElement("div");
-            node.classList.add('item');
-            node.setAttribute('id',i); //div增加id
-             //第一個參數event 第二個傳入自訂參數
-            /*
-             點擊後用id搜尋資料庫 問題?-> id固定在最後一個 
-             -> node.getAttribute('id') node停在最後一個
-             ->改成this.getAttribute('id')
-            */
-            node.addEventListener('click',addcart);
-            var IMG = document.createElement("img");
-            IMG.setAttribute("src", myJson[i].src);
-            var PRICE = document.createElement("p");
-            var NAME = document.createElement("p");
-            PRICE.innerHTML = "$" + myJson[i].price; //node p要用innnerHTML
-            NAME.innerHTML = myJson[i].name;
-            node.appendChild(IMG);
-            node.appendChild(PRICE);
-            node.appendChild(NAME);
-            items.appendChild(node); //加入DOM
-        }
-        function addcart(e){
-            var ID = e.currentTarget.getAttribute("id");
-            //e.currentTarget為事件監聽者
-            //e.target為事件發出者
-            var children = checkout.childNodes;
-            var checknumchildren = checkNum.childNodes;
-            var num = 1;
-            children[0].setAttribute("src",myJson[ID].src);
-            final[0].src = myJson[ID].src;
-            children[1].innerHTML = myJson[ID].name;
-            final[0].name = myJson[ID].name;
-            children[2].innerHTML = "$" + myJson[ID].price;
-            final[0].price = myJson[ID].price;
-            checknumchildren[2].innerHTML = num;
-            final[0].num = num;
-            checknumchildren[1].addEventListener('click',function(){
-                if(num>=2){
-                    num--;
-                    checknumchildren[2].innerHTML = num;
-                    final[0].num = num;
-                }
-            })
-            checknumchildren[3].addEventListener('click',function(){
-                num++;
+fetch('commodity.json')
+.then(function(response){
+    return response.json();
+})
+.then(function(myJson){
+    for(var i=0; i<myJson.length; i++){
+        var node = document.createElement("div");
+        node.classList.add('item');
+        node.setAttribute('id',i); //div增加id
+         //第一個參數event 第二個傳入自訂參數
+        /*
+         點擊後用id搜尋資料庫 問題?-> id固定在最後一個 
+         -> node.getAttribute('id') node停在最後一個
+         ->改成this.getAttribute('id')
+        */
+        node.addEventListener('click',addcart);
+        var IMG = document.createElement("img");
+        IMG.setAttribute("src", myJson[i].src);
+        var PRICE = document.createElement("p");
+        var NAME = document.createElement("p");
+        PRICE.innerHTML = "$" + myJson[i].price; //node p要用innnerHTML
+        NAME.innerHTML = myJson[i].name;
+        node.appendChild(IMG);
+        node.appendChild(PRICE);
+        node.appendChild(NAME);
+        items.appendChild(node); //加入DOM
+    }
+    function addcart(e){
+        var ID = e.currentTarget.getAttribute("id");
+        //e.currentTarget為事件監聽者
+        //e.target為事件發出者
+        var children = checkout.childNodes;
+        var checknumchildren = checkNum.childNodes;
+        var num = 1;
+        children[0].setAttribute("src",myJson[ID].src);
+        final[0].src = myJson[ID].src;
+        children[1].innerHTML = myJson[ID].name;
+        final[0].name = myJson[ID].name;
+        children[2].innerHTML = "$" + myJson[ID].price;
+        final[0].price = myJson[ID].price;
+        checknumchildren[2].innerHTML = num;
+        final[0].num = num;
+        checknumchildren[1].addEventListener('click',function(){
+            if(num>=2){
+                num--;
                 checknumchildren[2].innerHTML = num;
                 final[0].num = num;
-            })
-            checkButton.addEventListener('click',confirm);
-            $(checkoutBack).show();
-            checkout.style.bottom = "0";
-        }
-    })
-} 
+            }
+        })
+        checknumchildren[3].addEventListener('click',function(){
+            num++;
+            checknumchildren[2].innerHTML = num;
+            final[0].num = num;
+        })
+        checkButton.addEventListener('click',confirm);
+        $(checkoutBack).show();
+        checkout.style.bottom = "0";
+    }
+})
 
 checkoutBack.addEventListener('click',function(){
     $(checkoutBack).hide();
